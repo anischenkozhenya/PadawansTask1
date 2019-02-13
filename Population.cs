3b16899b3f699f0051a28a3bc6e933c1d750e014
anischenkozhenya <anischenkozhenya@gmail.com>
2019-02-13 21:02:33 +0300

    Update Population.cs

--- Population.cs
+++ Population.cs
@@ -6,8 +6,19 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (currentPopulation <= initialPopulation || initialPopulation < 0 || percent <= 0 || visitors < 0 || currentPopulation < 0)
+            {
+                throw new NotImplementedException();
+            }
+            int years = 0;
+            double population = initialPopulation;
+            do
+            {
+                population = population * (1 + (percent / 100)) + visitors;
+                years++;
+            }
+            while (population <= currentPopulation);
+            return years;
         }
     }
 }
